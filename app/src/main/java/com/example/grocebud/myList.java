f3a7dc57194da674bb288027e439753cd38c367b
nishchayjeet <nishchayjeets@gmail.com>
2020-11-22 22:17:47 -0800

    New

---
+++ app/src/main/java/com/example/grocebud/myList.java
@@ -0,0 +1,14 @@
+package com.example.grocebud;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class myList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_list);
+    }
+}
