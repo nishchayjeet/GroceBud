4cff18c194d397260ab84d55a88cbbe7e81b7d2e
nishchayjeet <nishchayjeets@gmail.com>
2020-12-01 10:46:12 -0800

    Data done

--- app/src/main/java/com/example/grocebud/mygrocery.java
+++ app/src/main/java/com/example/grocebud/mygrocery.java
@@ -5,6 +5,9 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.SearchView;
 
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.database.FirebaseDatabase;
@@ -42,4 +45,37 @@ import com.google.firebase.database.FirebaseDatabase;
          super.onStop();
          myadapter.stopListening();
      }
-}
+
+     @Override
+     public boolean onCreateOptionsMenu(Menu menu)
+     {
+         getMenuInflater().inflate(R.menu.searchmenu,menu);
+         MenuItem item = menu.findItem(R.id.search);
+         SearchView searchView = (SearchView)item.getActionView();
+         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+             @Override
+             public boolean onQueryTextSubmit(String query) {
+                 processsearch(query);
+                 return false;
+             }
+
+             @Override
+             public boolean onQueryTextChange(String newText) {
+                 processsearch(newText);
+                 return false;
+             }
+         });
+         return super.onCreateOptionsMenu(menu);
+     }
+
+     private void processsearch(String query) {
+         FirebaseRecyclerOptions<model> options =
+                 new FirebaseRecyclerOptions.Builder<model>()
+                         .setQuery(FirebaseDatabase.getInstance().getReference().child("Data").orderByChild("name").startAt(query).endAt(query+"\uf8ff"), model.class)
+                         .build();
+
+         myadapter = new myadapter(options);
+         myadapter.startListening();
+         recview.setAdapter(myadapter);
+     }
+ }
